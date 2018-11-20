66c1a350fd5ae712de64a84832a91f3003478a73
Thomas Gasparetto <thomas.gasparetto@ts.infn.it>
2018-11-20 15:03:22 +0100

    Addition to previous commit.

--- CREED_VTK/utils/__init__.py
+++ CREED_VTK/utils/__init__.py
@@ -1,5 +1,7 @@
 from .arrows import *
 from .colors import *
 from .cam_utils import *
+from .transf_utils import *
 
-__all__ = ['arrow_2d', 'MakeLUTFromCTF', 'get_cam_height']
+__all__ = ['arrow_2d', 'MakeLUTFromCTF', 'get_cam_height',
+           'create_arrow', 'scale_object', 'rotate_object', 'translate_object', 'create_extruded_text']
