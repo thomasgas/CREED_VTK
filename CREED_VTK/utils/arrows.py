57c789e94a905a5987aec81795af4ea03f559279
Thomas Gasparetto <thomas.gasparetto@ts.infn.it>
2018-11-20 15:02:03 +0100

    Arrows now scale properly. still need a bit of refactoring.

--- CREED_VTK/utils/arrows.py
+++ CREED_VTK/utils/arrows.py
@@ -1,5 +1,6 @@
 import vtk
 from .cam_utils import get_cam_height
+from .transf_utils import *
 
 
 def arrow_2d(event, tel_id, pointing):
@@ -28,13 +29,173 @@ def arrow_2d(event, tel_id, pointing):
     transform_arrow.Translate(get_cam_height(camera_type), 0.0, 0.0)
     transform_arrow.RotateY(90)
 
-    axes = vtk.vtkAxesActor()
-    axes.SetZAxisLabelText("")
-    axes.SetTotalLength(10, 10, 0)
-    axes.SetShaftTypeToCylinder()
-    axes.SetCylinderRadius(axes.GetCylinderRadius() * 0.8)
+    mbds = vtk.vtkMultiBlockDataSet()
+
+    arrows = 2
+
+    # allocate space for arrows and text
+    mbds.SetNumberOfBlocks(2 * arrows + 1)
+
+    # Leave block 1 NULL.  NULL blocks are valid and should be handled by
+    # algorithms that process multiblock datasets.  Especially when
+    # running in parallel where the blocks owned by other processes are
+    # NULL in this process.
+
+    arrow_length = 10
+    single_arrow = scale_object(create_arrow(), length=arrow_length)
+
+    X_arrow = single_arrow
+    Y_arrow = rotate_object(single_arrow, "z", 90)
+    # Z_arrow = rotate_object(single_arrow, "y", -90)
+
+    x_text = translate_object(create_extruded_text("X"), [arrow_length, 0, 0])
+
+    y_text = rotate_object(create_extruded_text("Y"), "z", 90)
+    y_text = translate_object(y_text, [0, arrow_length, 0])
+
+    # z_text = rotate_object(create_extruded_text("Z"), "y", -90)
+    # z_text = translate_object(z_text, [0, 0, arrow_length])
+    # Y_arrow = create_arrow_translated(0, 2 ,0)
+
+    mbds.SetBlock(0, X_arrow.GetOutput())
+    mbds.SetBlock(2, Y_arrow.GetOutput())
+    # mbds.SetBlock(3, Z_arrow.GetOutput())
+    mbds.SetBlock(3, x_text.GetOutput())
+    mbds.SetBlock(4, y_text.GetOutput())
+    # mbds.SetBlock(6, z_text.GetOutput())
+
+    mapper = vtk.vtkCompositePolyDataMapper2()
+    mapper.SetInputDataObject(mbds)
+
+    cdsa = vtk.vtkCompositeDataDisplayAttributes()
+    mapper.SetCompositeDataDisplayAttributes(cdsa)
+
+    # You can use the vtkCompositeDataDisplayAttributes to set the color,
+    # opacity and visibiliy of individual blocks of the multiblock dataset.
+    # Attributes are mapped by block pointers (vtkDataObject*), so these can
+    # be queried by their flat index through a convenience function in the
+    # attribute class (vtkCompositeDataDisplayAttributes::DataObjectFromIndex).
+    # Alternatively, one can set attributes directly through the mapper using
+    # flat indices.
+    #
+    # This sets the block at flat index 3 red
+    # Note that the index is the flat index in the tree, so the whole multiblock
+    # is index 0 and the blocks are flat indexes 1, 2 and 3.  This affects
+    # the block returned by mbds.GetBlock(2).
+    # double color[] = {1, 0, 0}
+
+    red = (255, 0, 0)
+    green = (0, 255, 0)
+    blue = (0, 0, 255)
+
+    mapper.SetBlockColor(1, red)
+    mapper.SetBlockColor(3, green)
+    # mapper.SetBlockColor(4, blue)
+    mapper.SetBlockColor(4, red)
+    mapper.SetBlockColor(5, green)
+    # mapper.SetBlockColor(7, blue)
+
+    axes = vtk.vtkActor()
+    axes.SetMapper(mapper)
     axes.SetUserTransform(transform_arrow)
-    axes.GetXAxisCaptionActor2D().GetTextActor().SetTextScaleMode(5)
-    axes.GetYAxisCaptionActor2D().GetTextActor().SetTextScaleMode(5)
+
+    return axes
+
+
+def arrow_3d(event, tel_id, pointing):
+    """
+    Create 2d arrow to be appended to the camera
+    :param event: event from simtel file
+    :param tel_id: tel_id given from the main.py loop
+    :param pointing: dictionary of dictionaries: first key is the tel_id
+                    second key are alt and az
+    :return: return axes actor placed at the right height above the mirrors plane
+    TODO: implement az rotation
+    """
+
+    tel_coords = event.inst.subarray.tel_coords
+
+    telescope = event.inst.subarray.tel[tel_id]
+    camera_type = telescope.camera.cam_id
+
+    tel_x_pos = tel_coords.x[tel_id - 1].value
+    tel_y_pos = tel_coords.y[tel_id - 1].value
+    tel_z_pos = tel_coords.z[tel_id - 1].value
+
+    transform_arrow = vtk.vtkTransform()
+    transform_arrow.Translate(tel_x_pos, tel_y_pos, tel_z_pos)
+    transform_arrow.RotateY(-pointing[tel_id]['alt'])
+    transform_arrow.Translate(get_cam_height(camera_type), 0.0, 0.0)
+    transform_arrow.RotateY(90)
+
+    mbds = vtk.vtkMultiBlockDataSet()
+
+    arrows = 3
+
+    # allocate space for arrows and text
+    mbds.SetNumberOfBlocks(2 * arrows + 1)
+
+    # Leave block 1 NULL.  NULL blocks are valid and should be handled by
+    # algorithms that process multiblock datasets.  Especially when
+    # running in parallel where the blocks owned by other processes are
+    # NULL in this process.
+
+    arrow_length = 10
+    single_arrow = scale_object(create_arrow(), length=arrow_length)
+
+    X_arrow = single_arrow
+    Y_arrow = rotate_object(single_arrow, "z", 90)
+    Z_arrow = rotate_object(single_arrow, "y", -90)
+
+    x_text = translate_object(create_extruded_text("X"), [arrow_length, 0, 0])
+
+    y_text = rotate_object(create_extruded_text("Y"), "z", 90)
+    y_text = translate_object(y_text, [0, arrow_length, 0])
+
+    z_text = rotate_object(create_extruded_text("Z"), "y", -90)
+    z_text = translate_object(z_text, [0, 0, arrow_length])
+    # Y_arrow = create_arrow_translated(0, 2 ,0)
+
+    mbds.SetBlock(0, X_arrow.GetOutput())
+    mbds.SetBlock(2, Y_arrow.GetOutput())
+    mbds.SetBlock(3, Z_arrow.GetOutput())
+    mbds.SetBlock(4, x_text.GetOutput())
+    mbds.SetBlock(5, y_text.GetOutput())
+    mbds.SetBlock(6, z_text.GetOutput())
+
+    mapper = vtk.vtkCompositePolyDataMapper2()
+    mapper.SetInputDataObject(mbds)
+
+    cdsa = vtk.vtkCompositeDataDisplayAttributes()
+    mapper.SetCompositeDataDisplayAttributes(cdsa)
+
+    # You can use the vtkCompositeDataDisplayAttributes to set the color,
+    # opacity and visibiliy of individual blocks of the multiblock dataset.
+    # Attributes are mapped by block pointers (vtkDataObject*), so these can
+    # be queried by their flat index through a convenience function in the
+    # attribute class (vtkCompositeDataDisplayAttributes::DataObjectFromIndex).
+    # Alternatively, one can set attributes directly through the mapper using
+    # flat indices.
+    #
+    # This sets the block at flat index 3 red
+    # Note that the index is the flat index in the tree, so the whole multiblock
+    # is index 0 and the blocks are flat indexes 1, 2 and 3.  This affects
+    # the block returned by mbds.GetBlock(2).
+    # double color[] = {1, 0, 0}
+
+    red = (255, 0, 0)
+    green = (0, 255, 0)
+    blue = (0, 0, 255)
+
+    mapper.SetBlockColor(1, red)
+    mapper.SetBlockColor(3, green)
+    mapper.SetBlockColor(4, blue)
+    mapper.SetBlockColor(5, red)
+    mapper.SetBlockColor(6, green)
+    mapper.SetBlockColor(7, blue)
+
+    axes = vtk.vtkActor()
+    axes.SetMapper(mapper)
+    axes.SetUserTransform(transform_arrow)
 
     return axes
---
+++ CREED_VTK/utils/transf_utils.py
@@ -0,0 +1,91 @@
+import vtk
+
+
+def create_arrow():
+    """
+    Create arrow: specify here all the parameters
+    :return: arrow
+    """
+    arrow = vtk.vtkArrowSource()
+    arrow.Update()
+
+    return arrow
+
+
+def translate_object(object, center):
+    """
+    Translate object
+    :param object: input object to translate
+    :param center: (x, y, z) coordinates of the center
+    :return:
+    """
+    transform_text = vtk.vtkTransform()
+    transform_text.Translate(center[0], center[1], center[2])
+    transform_text_axes = vtk.vtkTransformFilter()
+    transform_text_axes.SetTransform(transform_text)
+    transform_text_axes.SetInputConnection(object.GetOutputPort())
+    transform_text_axes.Update()
+    return transform_text_axes
+
+
+def scale_object(object, length):
+    """
+    Scale object
+    :param object: input object to scale
+    :param length: uniform scaling along all directions
+    :return:
+    """
+    transform_text = vtk.vtkTransform()
+    transform_text.Scale(length, length, length)
+    transform_text_axes = vtk.vtkTransformFilter()
+    transform_text_axes.SetTransform(transform_text)
+    transform_text_axes.SetInputConnection(object.GetOutputPort())
+    transform_text_axes.Update()
+    return transform_text_axes
+
+
+def rotate_object(object, axis, angle):
+    """
+    Rotate object around axis. These are the axes that are attached to the object
+    so that some rotation might bring no results. Check rotateWXVY for world coordinates (maybe).
+    :param object: is the vtkArrowSource Object (or another one)
+    :param axis: the rotation axis
+    :param angle: in degrees
+    :return: rotated object in the form of a transform filter (to be used later with GetOutput())
+    """
+    rotation_transform = vtk.vtkTransform()
+
+    if axis == "x":
+        rotation_transform.RotateX(angle)
+    elif axis == "y":
+        rotation_transform.RotateY(angle)
+    elif axis == "z":
+        rotation_transform.RotateZ(angle)
+
+    rotation_transform_filter = vtk.vtkTransformFilter()
+    rotation_transform_filter.SetTransform(rotation_transform)
+    rotation_transform_filter.SetInputConnection(object.GetOutputPort())
+    rotation_transform_filter.Update()
+    return rotation_transform_filter
+
+
+def create_extruded_text(text):
+    """
+    Create extruded text. To be then translated and rotated
+    :param text: input text. Useful for axes and telescopes labelling,
+    :return:
+    """
+    textSource = vtk.vtkVectorText()
+    textSource.SetText(text)
+    textSource.Update()
+
+    # Apply linear extrusion
+    extrude = vtk.vtkLinearExtrusionFilter()
+    extrude.SetInputConnection(textSource.GetOutputPort())
+    extrude.SetExtrusionTypeToNormalExtrusion()
+    extrude.SetVector(0, 0, 1)
+    extrude.SetScaleFactor(0.1)
+
+    triangleFilter = vtk.vtkTriangleFilter()
+    triangleFilter.SetInputConnection(extrude.GetOutputPort())
+    return triangleFilter
